192d392649e62cc9ba53eff738f20e1a56af1d21
crismmgg <cristinammgg@gmail.com>
2019-12-20 08:25:42 +0100

    add docs

---
+++ docs/precache-manifest.be39a47133da3c404561ecc427be7908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6353f007e6bc965b4264b35c89c37de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0664f57fe3b3c022d442",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a1dc4412fed9d9c002f",
+    "url": "/static/js/2.a9f61e5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a9f61e5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0664f57fe3b3c022d442",
+    "url": "/static/js/main.ee36c570.chunk.js"
+  },
+  {
+    "revision": "364592bd4ce4584cb924",
+    "url": "/static/js/runtime-main.995fe360.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
